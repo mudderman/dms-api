980c29a811f7ba04c27e7560b70a84a9d3fe01f4
Tomas Hau <tomas.hau3@ingka.ikea.com>
2022-09-09 12:25:03 +0200

    Initial project commit

---
+++ src/main/java/so/hau/dmsapi/DmsApiApplication.java
@@ -0,0 +1,11 @@
+package so.hau.dmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DmsApiApplication.class, args);
+    }
+}
---
+++ src/test/java/so/hau/dmsapi/DmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package so.hau.dmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
